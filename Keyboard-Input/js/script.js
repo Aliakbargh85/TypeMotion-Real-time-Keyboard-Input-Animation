b25b08c64df6b63d971b122562ff1fb602875ed2
Aliakbargh85 <131409388+Aliakbargh85@users.noreply.github.com>
2026-02-09 16:14:00 +0330

    Add files via upload

---
+++ Keyboard-Input/js/script.js
@@ -0,0 +1,42 @@
+﻿let $ = document;
+
+const titleElem = document.querySelector(".title");
+
+$.addEventListener("keyup", bodykeyup);
+
+function bodykeyup(e) {
+   appendValueToDom(e);
+
+   const userEventKey = e.key.toUpperCase(); // 'S A D'
+
+   const targetElement = $.getElementById(userEventKey);
+   // console.log(targetElement);
+
+   targetElement.classList.add("hit");
+
+   targetElement.addEventListener("animationend", function () {
+      targetElement.classList.remove("hit");
+   });
+   // console.log(targetElement);
+}
+
+function appendValueToDom(e) {
+   console.log(e);
+
+   if (e.code === "Backspace") {
+      titleElem.innerHTML = titleElem.innerHTML.slice(0, -1);
+      return;
+   }
+
+   if (e.key === "Escape") {
+      e.preventDefault();
+      return;
+   }
+
+   if (e.key === "Enter") {
+      e.preventDefault();
+      return;
+   }
+
+   titleElem.innerHTML += e.key;
+}
